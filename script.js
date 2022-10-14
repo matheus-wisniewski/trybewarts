14fe34deedbdfb93a96c0a9bda542197f01bacf9
Jefferson Bruno <bruunosoousaa@gmail.com>
2022-10-14 20:12:18 -0300

    JavaScript

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
+
+const inputPassword = document.getElementById("password");
 const btnEnviar = document.getElementById("btn-login");
-const inputEmail = documen.getElementById("email");
+const submitBtn = document.querySelector("#submit-btn");
+const agreement = document.querySelector("#agreement");
 
 
-function alerta {
 
+function alerta() {
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    if (email === "tryber@teste.com" && password === "123456") {
+        alert("Olá, Tryber!");
+    } else {
+        alert("Email ou senha inválidos.");
+    }
 }
+
+function enableSubmit() {
+    submitBtn.disabled = !agreement.checked;
+  }
+
+    btnEnviar.addEventListener("click", alerta);
+    agreement.addEventListener("change", enableSubmit);
+    
+
